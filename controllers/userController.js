53e00255d81471fd7ac6e78c7f8178c0cdc18730
Janette <janette.castillo91@yahoo.com>
2022-02-17 20:58:58 -0600

    updated user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,6 +26,24 @@ module.exports = {
         .catch((err) => res.status(500).json(err));
     },
 
+    // update a user 
+    updateUser(req, res) {
+        User.findByIdAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true }
+        )
+        .then((user) => 
+        !user
+        ? res.status(404).json({ message: 'No user with that UD'})
+        : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
     // Delete a user 
     deleteUser(req, res) { 
         User.findOneAndDelete({ _id: req.params.userId })
@@ -37,4 +55,34 @@ module.exports = {
         .then(() => res.json({ message: 'User and associated thoguhts deleted!'}))
         .catch((err)=> res.status(400).json(err))
     },
-}
+
+    // adds friend to user's friend list
+    addFriend(req,res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: 'No user with this ID'})
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // remove a friend from user list by ID
+    removeFriend(req, res) {
+        User.findByIdAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { responseId: req.params.friendId}  } }, 
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+        !user
+        ? res.status(400).json({ message: 'No user with that ID'} )
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+};
